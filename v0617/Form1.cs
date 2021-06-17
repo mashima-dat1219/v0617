c0ea2dc5288eba429c00b5e68cd915b8dae0a458
mashima-dat1219 <dat.gp1219@gmail.com>
2021-06-17 12:20:37 +0900

    作業

--- v0617/Form1.cs
+++ v0617/Form1.cs
@@ -14,6 +14,10 @@ namespace v0617
     {
         int vx = rand.Next(-10, 11);
         int vy = rand.Next(-10, 11);
+        int vx1 = rand.Next(-10, 11);
+        int vy1 = rand.Next(-10, 11);
+        int vx2 = rand.Next(-10, 11);
+        int vy2 = rand.Next(-10, 11);
         int score = 100;
         string temp, face = "('ω')";
         static Random rand = new Random();
@@ -24,6 +28,12 @@ namespace v0617
 
             label1.Left = rand.Next(ClientSize.Width - label1.Width);
             label1.Top = rand.Next(ClientSize.Height - label1.Height);
+
+            label4.Left = rand.Next(ClientSize.Width - label4.Width);
+            label4.Top = rand.Next(ClientSize.Height - label4.Height);
+
+            label5.Left = rand.Next(ClientSize.Width - label5.Width);
+            label5.Top = rand.Next(ClientSize.Height - label5.Height);
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -36,6 +46,12 @@ namespace v0617
 
             label1.Left += vx;
             label1.Top += vy;
+
+            label4.Left += vx1;
+            label4.Top += vy1;
+
+            label5.Left += vx2;
+            label5.Top += vy2;
 
             score--;
 
@@ -73,14 +89,79 @@ namespace v0617
                 face = temp;
             }
 
+            if (label4.Left < 0)
+            {
+                vx1 = Math.Abs(vx);
+            }
+
+            if (label4.Top < 0)
+            {
+                vy1 = Math.Abs(vy);
+            }
+
+            if (label4.Right > ClientSize.Width)
+            {
+                vx1 = -Math.Abs(vx);
+            }
+
+            if (label4.Bottom > ClientSize.Height)
+            {
+                vy1 = -Math.Abs(vy);
+            }
+
+            if (label5.Left < 0)
+            {
+                vx2 = Math.Abs(vx);
+            }
+
+            if (label5.Top < 0)
+            {
+                vy2 = Math.Abs(vy);
+            }
+
+            if (label5.Right > ClientSize.Width)
+            {
+                vx2 = -Math.Abs(vx);
+            }
+
+            if (label5.Bottom > ClientSize.Height)
+            {
+                vy2 = -Math.Abs(vy);
+            }
+
             if (    (fpos.X >= label1.Left)
                 &&  (fpos.X < label1.Right)
                 &&  (fpos.Y >= label1.Top)
                 &&  (fpos.Y < label1.Bottom)
                 )
             {
-                timer1.Enabled = false;
+                //timer1.Enabled = false;
+                label1.Visible = false;
             }
+
+            //
+            if ((fpos.X >= label4.Left)
+                && (fpos.X < label4.Right)
+                && (fpos.Y >= label4.Top)
+                && (fpos.Y < label4.Bottom)
+                )
+            {
+                //timer1.Enabled = false;
+                label4.Visible = false;
+            }
+
+            //
+            if ((fpos.X >= label5.Left)
+                && (fpos.X < label5.Right)
+                && (fpos.Y >= label5.Top)
+                && (fpos.Y < label5.Bottom)
+                )
+            {
+                //timer1.Enabled = false;
+                label5.Visible = false;
+            }
+
+
         }
 
         private void label1_Click(object sender, EventArgs e)
