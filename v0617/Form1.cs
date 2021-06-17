01cdf7cd1b0b13f9c80356643eb77376e20a2497
mashima-dat1219 <dat.gp1219@gmail.com>
2021-06-17 09:41:49 +0900

    確認問題

--- v0617/Form1.cs
+++ v0617/Form1.cs
@@ -12,10 +12,47 @@ namespace v0617
 {
     public partial class Form1 : Form
     {
+        int vx = -10, vy = -10;
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+
+            if (label1.Left < 0)
+            {
+                vx = Math.Abs(vx);
+            }
+
+            if (label1.Top < 0)
+            {
+                vy = Math.Abs(vy);
+            }
+
+            if (label1.Right > ClientSize.Width)
+            {
+                vx = -Math.Abs(vx);
+            }
+
+            if (label1.Bottom > ClientSize.Height)
+            {
+                vy = -Math.Abs(vy);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
